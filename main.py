04edb7e7aff1c9bd86bcccbcb6742a6935318779
Dootam Roy <dootamroy22@gmail.com>
2021-07-11 04:41:56 +0530

    .

--- main.py
+++ main.py
@@ -58,11 +58,11 @@ while True:
             name = StudentNames[matchIndex].upper()
             # print(name)
             y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4          # cos we have earlier resized the images to 1/4th.
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            # markAttendance(name)
+            markAttendance(name)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) & 0xff == ord('q'):
