243d668c31a1515070124d644cf6795fc1f72cf5
Dootam Roy <dootamroy22@gmail.com>
2021-07-11 04:54:21 +0530

    date

--- markAttendance.py
+++ markAttendance.py
@@ -1,9 +1,14 @@
 # created by Dootam Roy @2021
 
 from datetime import datetime
+from datetime import date
+
+today = date.today()
 
 
 def markAttendance(name):
+
+    # ..............saving in CSV file................#
     with open('Attendance.csv', 'r+') as f:
         myDataList = f.readlines()
         nameList = []
@@ -13,5 +18,10 @@ def markAttendance(name):
 
         if name not in nameList:
             now = datetime.now()
+            date = today.strftime("%d/%m/%Y")
             dtString = now.strftime('%H:%M:%S')
-            f.writelines(f'\n{name},{dtString}')          # saves the name and time in the attendance.csv file.
+            f.writelines(f'\n{name}, {date}, {dtString}')          # saves the name and time in the attendance.csv file.
+
+    # .............saving in FIREBASE.................#
+
+    
