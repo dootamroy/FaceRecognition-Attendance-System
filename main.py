ae9e9468ab0c310597f7ae9268594eac97a2c39a
Dootam Roy <dootamroy22@gmail.com>
2021-07-11 07:50:24 +0530

    unknon recognise

--- main.py
+++ main.py
@@ -54,15 +54,17 @@ while True:
         # print(faceDis)
         matchIndex = np.argmin(faceDis)      # for finding index from the list faceDist which has the least distance.
 
-        if matches[matchIndex]:
+        if faceDis[matchIndex]< 0.5:
             name = StudentNames[matchIndex].upper()
             # print(name)
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4          # cos we have earlier resized the images to 1/4th.
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            markAttendance(name)
+        else:
+            name = 'Unknown'
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4          # cos we have earlier resized the images to 1/4th.
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+        markAttendance(name)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) & 0xff == ord('q'):
